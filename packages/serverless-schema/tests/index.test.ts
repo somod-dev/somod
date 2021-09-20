110ec3b30a1c689a97ea0474248a11792d557baf
Raghavendra K R <krraghavendra@yahoo.co.in>
2021-09-20 12:17:14 +0530

    added DefinitionBody to API Resource

--- packages/serverless-schema/tests/index.test.ts
+++ packages/serverless-schema/tests/index.test.ts
@@ -1,9 +1,11 @@
 import Ajv from "ajv";
 import { loadSchemas } from "../src";
+import addFormats from "ajv-formats";
 
 describe("Test index.loadSchemas", () => {
   test("all schemas will load successfully", async () => {
     const ajv = new Ajv();
+    addFormats(ajv);
     const validate = await loadSchemas(ajv);
     expect(validate.errors).toBeNull();
   });
