890da157487cd6f11b17a8c8e49cd251b9708937
Raghavendra K R <krraghavendra@yahoo.co.in>
2021-09-30 17:32:13 +0530

    Closed #5

---
+++ packages/emp/README.md
@@ -0,0 +1,14 @@
+# `@sodaru/emp`
+
+> Enhances NPM Package to be Entranse Module Package (EMP)
+
+This is a combination of [njp](../njp) and [slp](../slp)
+
+## Usage
+
+```SH
+ > emp init
+ > emp build
+ > emp deploy
+ > emp serve
+```
---
+++ packages/emp/src/commands/serve.ts
@@ -0,0 +1,87 @@
+import { CommonOptions, taskRunner } from "@sodaru-cli/base";
+import {
+  createPages,
+  createPublicAssets,
+  createRootModulePages,
+  createRootModulePublicAssets,
+  key_njp,
+  path_pages,
+  path_public,
+  path_ui,
+  startNextDev,
+  watchRootModulePages,
+  watchRootModulePublicAssets
+} from "@sodaru-cli/package-manager-lib";
+import { Command, Option } from "commander";
+import { BuildAction } from "./build";
+
+type ServeOptions = CommonOptions & {
+  stage: "all" | "prepare" | "start-dev";
+};
+
+export const ServeAction = async ({
+  verbose,
+  stage
+}: ServeOptions): Promise<void> => {
+  const dir = process.cwd();
+
+  if (stage == "all" || stage == "prepare") {
+    await BuildAction({ verbose, type: "njp" });
+
+    await Promise.all([
+      taskRunner(`Create ${path_pages}`, createPages, verbose, dir, [key_njp]),
+      taskRunner(`Create ${path_public}`, createPublicAssets, verbose, dir, [
+        key_njp
+      ])
+    ]);
+
+    await Promise.all([
+      taskRunner(
+        `Update ${path_pages} with ${path_ui}/${path_pages}`,
+        createRootModulePages,
+        verbose,
+        dir
+      ),
+      taskRunner(
+        `Update ${path_public} with ${path_ui}/${path_public}`,
+        createRootModulePublicAssets,
+        verbose,
+        dir
+      )
+    ]);
+  }
+
+  if (stage == "all" || stage == "start-dev") {
+    await taskRunner(
+      `Watch ${path_ui}/${path_pages}`,
+      watchRootModulePages,
+      verbose,
+      dir
+    );
+    await taskRunner(
+      `Watch ${path_ui}/${path_public}`,
+      watchRootModulePublicAssets,
+      verbose,
+      dir
+    );
+
+    await taskRunner(
+      `Start NextJs Development server`,
+      startNextDev,
+      verbose,
+      dir
+    );
+  }
+};
+
+const serveCommand = new Command("dev");
+
+serveCommand.action(ServeAction);
+
+serveCommand.addOption(
+  new Option("-s, --stage [stage]", "Serve stages to execute")
+    .choices(["all", "prepare", "start-dev"])
+    .default("all")
+);
+
+export default serveCommand;
