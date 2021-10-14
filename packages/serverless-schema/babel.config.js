ed94bdea8bc44c76c95ce299d44e4d77f6b90dbb
Raghavendra K R <krraghavendra@yahoo.co.in>
2021-10-14 13:38:24 +0530

    moved serverless-schema out , Fixing #33

--- packages/serverless-schema/babel.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-/* AUTO-CREATED by sodev jest (more info at @sodaru-cli/sodev)*/
-
-const commonConfig = require("@sodaru/jest-config/babel.config");
-
-module.exports = {
-  ...commonConfig
-};
