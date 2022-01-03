48524db462179d517ac68d54026708b43a233695
Raghavendra K R <raghavendra@sodaru.com>
2022-01-03 22:27:11 +0530

    updated lerna-utilities to fix peerDependencies

--- packages/common-lib/src/schema.ts
+++ packages/common-lib/src/schema.ts
@@ -1,4 +1,7 @@
-export const moduleNameSchema = {
-  pattern: "(@[a-z0-9\\-]+\\/)?[a-z0-9\\-]+",
+import { JSONSchema7Definition } from "json-schema";
+
+export const moduleNameSchema: JSONSchema7Definition = {
+  type: "string",
+  pattern: "^(@[a-z0-9\\-]+\\/)?[a-z0-9\\-]+$",
   maxLength: 128
 };
