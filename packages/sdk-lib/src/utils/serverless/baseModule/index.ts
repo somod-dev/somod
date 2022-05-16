d20bf68cb539285d637d71d7977ada3eccbef13a
Raghavendra K R <raghavendra@sodaru.com>
2022-05-16 18:45:11 +0530

    removed http and customresource layers from serverless utils

--- packages/sdk-lib/src/utils/serverless/baseModule/index.ts
+++ packages/sdk-lib/src/utils/serverless/baseModule/index.ts
@@ -1,28 +1,16 @@
 import { findReferences } from "../keywords/ref";
 import { OriginalSLPTemplate, ServerlessTemplate } from "../types";
 import { baseLayerName, getBaseLayerSLPResource } from "./layers/baseLayer";
-import {
-  customResourceLayerName,
-  getCustomResourceLayerSLPResource
-} from "./layers/customResourceLayer";
-import {
-  httpWrapperLayerName,
-  gethttpWrapperLayer
-} from "./layers/httpWrapperLayer";
 
 export const baseModuleName = "@somod/slp";
 
 export const getBaseModuleOriginalSLPTemplate =
   async (): Promise<OriginalSLPTemplate> => {
     const baseLayer = await getBaseLayerSLPResource();
-    const customResourceLayer = await getCustomResourceLayerSLPResource();
-    const httpWrapperLayer = await gethttpWrapperLayer();
 
     const baseModule = {
       Resources: {
-        [baseLayerName]: baseLayer,
-        [customResourceLayerName]: customResourceLayer,
-        [httpWrapperLayerName]: httpWrapperLayer
+        [baseLayerName]: baseLayer
       }
     };
     return baseModule;
@@ -36,25 +24,5 @@ export const cleanUpBaseModule = (serverlessTemplate: ServerlessTemplate) => {
   });
   if (Object.keys(baseLayerReferences).length == 0) {
     delete serverlessTemplate[baseModuleName].Resources[baseLayerName];
-  }
-
-  // clean customResourceLayer
-  const customResourceLayerReferences = findReferences(serverlessTemplate, {
-    module: baseModuleName,
-    resource: customResourceLayerName
-  });
-  if (Object.keys(customResourceLayerReferences).length == 0) {
-    delete serverlessTemplate[baseModuleName].Resources[
-      customResourceLayerName
-    ];
-  }
-
-  // clean httpWrapperLayer
-  const httpWrapperLayerReferences = findReferences(serverlessTemplate, {
-    module: baseModuleName,
-    resource: httpWrapperLayerName
-  });
-  if (Object.keys(httpWrapperLayerReferences).length == 0) {
-    delete serverlessTemplate[baseModuleName].Resources[httpWrapperLayerName];
   }
 };
