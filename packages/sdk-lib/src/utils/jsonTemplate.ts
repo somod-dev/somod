dcc8c46bc377fe3315cd913e07548b5d700950c1
Raghavendra K R <raghavendra@sodaru.com>
2022-08-23 09:21:10 +0530

    added jsonTemplate to parse and replace keywords within a json

---
+++ packages/sdk-lib/src/utils/jsonTemplate.ts
@@ -0,0 +1,238 @@
+import {
+  intersection,
+  isArray,
+  isBoolean,
+  isNull,
+  isNumber,
+  isPlainObject,
+  isString
+} from "lodash";
+
+export type JSONBaseNode = Readonly<{
+  parent?: {
+    key: number | string;
+    node: JSONArrayNode | JSONObjectNode;
+  };
+}>;
+
+export type JSONPrimitiveNode = Readonly<{
+  type: "primitive";
+  value: JSONPrimitiveType;
+}> &
+  JSONBaseNode;
+
+export type JSONArrayNode = Readonly<{
+  type: "array";
+  items: JSONNode[];
+}> &
+  JSONBaseNode;
+
+export type JSONObjectNode = Readonly<{
+  type: "object";
+  properties: Record<string, JSONNode>;
+}> &
+  JSONBaseNode;
+
+export type JSONNode = JSONPrimitiveNode | JSONArrayNode | JSONObjectNode;
+
+export type JSONPrimitiveType = string | boolean | number | null;
+
+export type JSONArrayType = JSONType[];
+
+export type JSONObjectType = {
+  [property: string]: JSONType;
+};
+
+export type JSONType = JSONPrimitiveType | JSONArrayType | JSONObjectType;
+
+export const parseJson = (json: JSONType): JSONNode => {
+  const navigator = (json: JSONType, parent?: JSONNode["parent"]): JSONNode => {
+    if (isArray(json)) {
+      const itemNodes: JSONNode[] = [];
+      const thisNode: JSONNode = {
+        type: "array",
+        items: itemNodes,
+        parent: parent
+      };
+      Object.freeze(thisNode);
+      json.forEach((item, i) => {
+        const itemNode = navigator(item, {
+          key: i,
+          node: thisNode
+        });
+        itemNodes.push(itemNode);
+      });
+      Object.freeze(itemNodes);
+      return thisNode;
+    } else if (isPlainObject(json)) {
+      const propertyNodes: Record<string, JSONNode> = {};
+      const thisNode: JSONNode = {
+        type: "object",
+        properties: propertyNodes,
+        parent: parent
+      };
+      Object.freeze(thisNode);
+      Object.keys(json).forEach(property => {
+        const propertyNode = navigator(json[property], {
+          key: property,
+          node: thisNode
+        });
+        propertyNodes[property] = propertyNode;
+      });
+      Object.freeze(propertyNodes);
+      return thisNode;
+    } else if (
+      isString(json) ||
+      isBoolean(json) ||
+      isNumber(json) ||
+      isNull(json)
+    ) {
+      const thisNode: JSONNode = {
+        type: "primitive",
+        value: json,
+        parent: parent
+      };
+      Object.freeze(thisNode);
+      return thisNode;
+    } else {
+      throw new Error(`Unknown type ${json} ${parent}`);
+    }
+  };
+  return navigator(json);
+};
+
+export const constructJson = (jsonNode: JSONNode): JSONType => {
+  if (jsonNode.type == "primitive") {
+    return jsonNode.value;
+  } else if (jsonNode.type == "array") {
+    return jsonNode.items.map(item => constructJson(item));
+  } else if (jsonNode.type == "object") {
+    return Object.fromEntries(
+      Object.keys(jsonNode.properties).map(property => [
+        property,
+        constructJson(jsonNode.properties[property])
+      ])
+    );
+  }
+};
+
+export type KeywordValidator = (
+  keyword: string,
+  node: JSONObjectNode,
+  value: JSONType
+) => Error[];
+
+export const validateKeywords = (
+  jsonNode: JSONNode,
+  keywordValidators: Record<string, KeywordValidator>
+): Error[] => {
+  const errors: Error[] = [];
+  const keywords = Object.keys(keywordValidators);
+  const navigator = (jsonNode: JSONNode) => {
+    if (jsonNode.type == "array") {
+      jsonNode.items.forEach(item => {
+        navigator(item);
+      });
+    } else if (jsonNode.type == "object") {
+      const properties = Object.keys(jsonNode.properties);
+      properties.forEach(property => {
+        navigator(jsonNode.properties[property]);
+      });
+
+      // validation
+      const keywordsInThisObject = intersection(properties, keywords);
+      keywordsInThisObject.forEach(keyword => {
+        const keywordErrors = keywordValidators[keyword](
+          keyword,
+          jsonNode,
+          constructJson(jsonNode.properties[keyword])
+        );
+        errors.push(...keywordErrors);
+      });
+    }
+  };
+  navigator(jsonNode);
+  return errors;
+};
+
+export type KeywordObjectReplacement = {
+  type: "object";
+  value: JSONType;
+};
+
+export type KeywordKeywordReplacement = {
+  type: "keyword";
+  value: JSONObjectType;
+};
+
+export type KeywordReplacement =
+  | KeywordObjectReplacement
+  | KeywordKeywordReplacement;
+
+export type KeywordProcessor = (
+  keyword: string,
+  node: JSONObjectNode,
+  value: JSONType
+) => KeywordReplacement;
+
+export const processKeywords = (
+  jsonNode: JSONNode,
+  keywordProcessors: Record<string, KeywordProcessor>
+): JSONType => {
+  const keywords = Object.keys(keywordProcessors);
+  const navigator = (jsonNode: JSONNode): JSONType => {
+    if (jsonNode.type == "primitive") {
+      return jsonNode.value;
+    } else if (jsonNode.type == "array") {
+      const jsonArray = jsonNode.items.map(item => navigator(item));
+      Object.freeze(jsonArray);
+      return jsonArray;
+    } else if (jsonNode.type == "object") {
+      const properties = Object.keys(jsonNode.properties);
+      const jsonObject = Object.fromEntries(
+        properties.map(property => [
+          property,
+          navigator(jsonNode.properties[property])
+        ])
+      );
+
+      // replacement
+      const keywordsInThisObject = intersection(properties, keywords);
+      const keywordReplacements = keywordsInThisObject.map(keyword => {
+        const replacer = keywordProcessors[keyword](
+          keyword,
+          jsonNode,
+          jsonObject[keyword]
+        );
+        return { ...replacer, keyword };
+      });
+      const keywordObjectReplacements = keywordReplacements.filter(
+        r => r.type == "object"
+      );
+      if (keywordObjectReplacements.length > 1) {
+        throw new Error(
+          `Object replacement is allowed for only one keyword, Found ${keywordObjectReplacements.map(
+            r => r.keyword
+          )}`
+        );
+      }
+
+      let resultObject =
+        keywordObjectReplacements.length == 1
+          ? keywordObjectReplacements[0].value
+          : jsonObject;
+
+      if (isPlainObject(resultObject)) {
+        keywordReplacements.forEach(r => {
+          if (r.type == "keyword") {
+            delete resultObject[r.keyword];
+            resultObject = { ...(resultObject as JSONObjectType), ...r.value };
+          }
+        });
+      }
+      Object.freeze(resultObject);
+      return resultObject;
+    }
+  };
+  return navigator(jsonNode);
+};
