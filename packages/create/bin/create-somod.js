88503f936140ce9274a0b435df416a24e9b0cd55
Raghavendra K R <raghavendra@sodaru.com>
2022-10-01 10:45:21 +0530

    corrected open telemetry and fixed issue in create-somod

--- packages/create/bin/create-somod.js
+++ packages/create/bin/create-somod.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 /* eslint-disable */
 process.env.CREATE_SOMOD_CLI_PATH = require("path").dirname(__dirname);
-require("../dist/index.js");
+
+require("cli-opentelemetry").tele(
+  "create-somod",
+  require("path").join(__dirname, "../dist/index.js"),
+  "VERSION",
+  "OTLP_URL",
+  {
+    // prettier-ignore
+    "OTLP_KEY_NAME": "OTLP_KEY_VALUE"
+  },
+  3 * 60 * 1000 // 3 minutes
+);
--- packages/somod/bin/somod.js
+++ packages/somod/bin/somod.js
@@ -3,9 +3,11 @@
 require("cli-opentelemetry").tele(
   "somod",
   require("path").join(__dirname, "../dist/index.js"),
+  "VERSION",
   "OTLP_URL",
   {
     // prettier-ignore
     "OTLP_KEY_NAME": "OTLP_KEY_VALUE"
-  }
+  },
+  2 * 60 * 1000 // 2 minutes
 );
