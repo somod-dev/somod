9267d1afe7ba85e3be7183af849eef6858fcbfba
Raghavendra K R <raghavendra@sodaru.com>
2023-02-11 14:47:49 +0530

    fix a test in lib

--- packages/lib/tests/utils/extension/bundle.test.ts
+++ packages/lib/tests/utils/extension/bundle.test.ts
@@ -33,12 +33,22 @@ describe("Test util extension.bundle", () => {
     });
     await expect(bundle({ dir } as IContext)).resolves.toBeUndefined();
 
-    await expect(
-      readFile(join(dir, "build/extension.js"), "utf8")
-    ).resolves.toContain(
-      `var t=Object.defineProperty;var d=Object.getOwnPropertyDescriptor;var i=Object.getOwnPropertyNames;var l=Object.prototype.hasOwnProperty;var n=o=>t(o,"__esModule",{value:!0});var s=(o,e)=>{for(var p in e)t(o,p,{get:e[p],enumerable:!0})},u=(o,e,p,c)=>{if(e&&typeof e=="object"||typeof e=="function")for(let r of i(e))!l.call(o,r)&&(p||r!=="default")&&t(o,r,{get:()=>e[r],enumerable:!(c=d(e,r))||c.enumerable});return o};var x=(o=>(e,p)=>o&&o.get(e)||(p=u(n({}),e,1),o&&o.set(e,p),p))(typeof WeakMap!="undefined"?new WeakMap:0);var a={};s(a,{preBuild:()=>B});var B=10;module.exports=x(a);0&&(module.exports={preBuild});
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vLi4vLi4vLi4vLi4vLi4vLi4vcHJpdmF0ZS92YXIvZm9sZGV`
+    const bundledExtension = await readFile(
+      join(dir, "build/extension.js"),
+      "utf8"
     );
+    const bundledExtensionLines = bundledExtension.split("\n");
+    expect(bundledExtensionLines.length).toEqual(3);
+
+    expect(bundledExtensionLines[0]).toEqual(
+      `var t=Object.defineProperty;var d=Object.getOwnPropertyDescriptor;var i=Object.getOwnPropertyNames;var l=Object.prototype.hasOwnProperty;var n=o=>t(o,"__esModule",{value:!0});var s=(o,e)=>{for(var p in e)t(o,p,{get:e[p],enumerable:!0})},u=(o,e,p,c)=>{if(e&&typeof e=="object"||typeof e=="function")for(let r of i(e))!l.call(o,r)&&(p||r!=="default")&&t(o,r,{get:()=>e[r],enumerable:!(c=d(e,r))||c.enumerable});return o};var x=(o=>(e,p)=>o&&o.get(e)||(p=u(n({}),e,1),o&&o.set(e,p),p))(typeof WeakMap!="undefined"?new WeakMap:0);var a={};s(a,{preBuild:()=>B});var B=10;module.exports=x(a);0&&(module.exports={preBuild});`
+    );
+    expect(
+      bundledExtensionLines[1].startsWith(
+        "//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cm"
+      )
+    ).toBeTruthy();
+    expect(bundledExtensionLines[2]).toEqual("");
   });
 
   test("For overrided bundleConfig", async () => {
